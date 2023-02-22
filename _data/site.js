e5fd9f40cbea3532018ea8ab52fd3eb173534638
Joseph Diodato <jadiodato@gmail.com>
2023-02-22 08:33:46 -0500

    Update site.js

--- _data/site.js
+++ _data/site.js
@@ -1,20 +1,21 @@
 module.exports = {
   meta: {
-    title: "My Purple Blog",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    title: "The Punk Pedagogue",
+    description: "",
     lang: "en",
-    siteUrl: "https://example.com/",
+    siteUrl: "thepunkpedagogue.com",
   },
   feed: { // used in feed.xml.njk
     subtitle: "Lorem ipsum dolor sit amet consecuteor",
     filename: "atom.xml",
     path: "/atom.xml",
-    id: "https://example.com/",
-    authorName: "John Doe",
-    authorEmail: "johndoe@example.com"
+    id: "thepunkpedagogue.com",
+    authorName: "Joe Diodato",
+    authorEmail: "diodatouft@gmail.com"
   },
   hero: { // used in hero section of main page ie. index.html.njk
-    title: "Welcome to my purple blog",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusantium deserunt odio esse."
+    title: "Welcome to the Punk Pedagogue",
+    description: "My name is Joe Diodato. I'm a special education teacher, unionist, and musician. This blog is a place for me to document my learning and growth as an educator and unionist" +
+    "Help yourself to a cup of tea, and thanks for stopping by my little corner of the internet. 
   }
 }
